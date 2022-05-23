680b7e761ecc7746bff5740e481b933bcc4a3213
Klaven Jones <klavensjones@gmail.com>
2022-05-23 14:28:16 -0400

    Initial tests and implementations for Messages class

--- lib/message.rb
+++ lib/message.rb
@@ -2,6 +2,18 @@ class Message
   def welcome
     "-----------------------------------------------------\nWelcome To Tic Tac Toe\n-----------------------------------------------------"
   end
+
+  def instruction
+    "Please pick a spot by pressing one number in the range of 1-9"
+  end
+
+  def winner(player)
+    "Player #{player} is the winner."
+  end
+
+  def tie
+    "The game has ended in a tie."
+  end
 end
 
 message = Message.new
--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -11,4 +11,35 @@ describe Message do
     end
   end
 
+  describe "#winner" do
+    it "should return the correct message when player X is the winner" do
+      message = Message.new
+      output = message.winner("X")
+      expect(output).to eq("Player X is the winner.")
+    end
+
+    it "should return the correct message when player O is the winner" do
+      message = Message.new
+      output = message.winner("O")
+      expect(output).to eq("Player O is the winner.")
+    end
+  end
+
+  describe "#tie" do
+    it "should return that the game ended in a tie" do
+      message = Message.new
+      output = message.tie
+      expect(output).to eq("The game has ended in a tie.")
+    end
+  end
+
+  describe "#instruction" do
+    it "should return instructions to make a move" do
+      message = Message.new
+      output = message.instruction
+      expect(output).to eq(
+        "Please pick a spot by pressing one number in the range of 1-9"
+      )
+    end
+  end
 end
